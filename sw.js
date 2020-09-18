0d539c179d7f0299d53d04f8c5489723d97425ed
Israel Lage <lage.israel@hotmail.com>
2020-09-18 13:07:28 +0000

    Updates

--- sw.js
+++ sw.js
@@ -26,7 +26,7 @@ workbox.clientsClaim();
  */
 self.__precacheManifest = [
   {
-    "url": "webpack-runtime-78ea72549238b8abff6f.js"
+    "url": "webpack-runtime-848e6cebf70d37522c70.js"
   },
   {
     "url": "styles.1f51b7a7bda2584f3155.css"
@@ -35,7 +35,7 @@ self.__precacheManifest = [
     "url": "styles-7f9f4c7e17b2b76e32b5.js"
   },
   {
-    "url": "commons-81aba4a304aeb7543647.js"
+    "url": "commons-7c03d4ef725038b5bf5d.js"
   },
   {
     "url": "netlify-identity-widget-8c16a5a2537f2f2b6a08.js"
@@ -48,7 +48,7 @@ self.__precacheManifest = [
   },
   {
     "url": "offline-plugin-app-shell-fallback/index.html",
-    "revision": "2cf8939fc7d86c7cbe68ba08e673f308"
+    "revision": "fabe09bcb4404d8d287879ad2cfafba5"
   },
   {
     "url": "manifest.webmanifest",
